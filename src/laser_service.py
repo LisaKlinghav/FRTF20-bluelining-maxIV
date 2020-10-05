fcf54eba7af0a5fd1830b4918ed78abefb4ada2a
Kim Haapamaki <kimhaafi@gmail.com>
2020-10-05 13:55:19 +0000

    added currentPos and nextPose in laserService

--- src/laser_service.py
+++ src/laser_service.py
@@ -5,29 +5,21 @@ from nav_msgs.msg import Odometry
 from FRTF20blueliningmaxIV.srv import laser_serviceResponse, laser_service
 
 def callback(msg):
-    # TODO: Get laserdata to determin our location
-    robotCurr = robotOdom.pose.pose
-    if msg.find:
-        robotNext = robotOdom.pose.pose
-        robotNext.position.x += 1
-
     response = laser_serviceResponse()
+    # For testing purposes, later we want the laser data instead
+    # Gets the robot location in the enviroment
+    robotOdom = rospy.wait_for_message('/odom', Odometry)
+
+    # TODO: Get laserdata to determin our location
+    response.currentPos = robotOdom.pose.pose
+    if msg.find:
+        response.nextPos = robotOdom.pose.pose
+        response.nextPos.position.x = 1
+
     response.success = True
     return response
 
 rospy.init_node('laser_service', anonymous=True)
 srv = rospy.Service('laser_service', laser_service, callback)
 
-# For testing purposes, later we want the laser data instead
-# Gets the robot location in the enviroment
-robotOdom = Odometry()
-def positionCallback(msg):
-    robotOdom = msg
-
-robot_odom = rospy.Subscriber('/odom', Odometry, positionCallback)
-
-r = rospy.Rate(10) # 10hz
-while not rospy.is_shutdown():
-    currPos.publish(robotCurr)
-    nextPos.publish(robotNext)
-    r.sleep()
+rospy.spin()
